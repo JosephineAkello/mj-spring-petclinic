4f702c8818dbd7d48a11b2124881006324feafb0
Maureen Josephine <josephineakello2030@gmail.com>
2020-04-05 01:23:50 +0300

    generate getters and setters

--- src/main/java/com/example/mjspringpetclinic/model/Pet.java
+++ src/main/java/com/example/mjspringpetclinic/model/Pet.java
@@ -1,4 +1,34 @@
 package com.example.mjspringpetclinic.model;
 
+import java.time.LocalDateTime;
+
 public class Pet {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDateTime birthdate;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDateTime getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDateTime birthdate) {
+        this.birthdate = birthdate;
+    }
 }
