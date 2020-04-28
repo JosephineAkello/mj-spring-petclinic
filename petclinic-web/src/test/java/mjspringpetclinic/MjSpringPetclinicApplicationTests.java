e369612cbb412015b963f312f08f9bf055b7b89a
Maureen Josephine <m.akelo@carepay.com>
2020-04-28 21:00:54 +0300

    add context configuration

--- petclinic-web/src/test/java/mjspringpetclinic/MjSpringPetclinicApplicationTests.java
+++ petclinic-web/src/test/java/mjspringpetclinic/MjSpringPetclinicApplicationTests.java
@@ -1,9 +1,13 @@
 package mjspringpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@SpringBootConfiguration
+@ContextConfiguration
 class MjSpringPetclinicApplicationTests {
 
 	@Test
