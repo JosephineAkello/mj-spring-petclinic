0a28affa8a839aa116a63910fb22ae0abf469f0f
Josephine Akello <josephineakello2030@gmail.com>
2020-05-01 12:58:07 +0300

    rename objects and remove un used imports

--- petclinic-data/src/main/java/com/example/mjspringpetclinic/service/map/PetServiceMap.java
+++ petclinic-data/src/main/java/com/example/mjspringpetclinic/service/map/PetServiceMap.java
@@ -2,28 +2,28 @@ package com.example.mjspringpetclinic.service.map;
 
 import java.util.Set;
 
-import com.example.mjspringpetclinic.model.Owner;
+import com.example.mjspringpetclinic.model.Pet;
 import com.example.mjspringpetclinic.service.CrudService;
 
-public class PetServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
 
     @Override
-    public Set<Owner> findAll() {
+    public Set<Pet> findAll() {
         return super.findAll();
     }
 
     @Override
-    public Owner findById(Long id) {
+    public Pet findById(Long id) {
         return super.findById(id);
     }
 
     @Override
-    public Owner save(Owner object) {
+    public Pet save(Pet object) {
         return super.save(object.getId(), object);
     }
 
     @Override
-    public void delete(Owner object) {
+    public void delete(Pet object) {
         super.delete(object);
     }
 
--- petclinic-data/src/main/java/com/example/mjspringpetclinic/service/map/VetServiceMap.java
+++ petclinic-data/src/main/java/com/example/mjspringpetclinic/service/map/VetServiceMap.java
@@ -2,28 +2,28 @@ package com.example.mjspringpetclinic.service.map;
 
 import java.util.Set;
 
-import com.example.mjspringpetclinic.model.Owner;
+import com.example.mjspringpetclinic.model.Vet;
 import com.example.mjspringpetclinic.service.CrudService;
 
-public class VetServiceMap  extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class VetServiceMap  extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
 
     @Override
-    public Set<Owner> findAll() {
+    public Set<Vet> findAll() {
         return super.findAll();
     }
 
     @Override
-    public Owner findById(Long id) {
+    public Vet findById(Long id) {
         return super.findById(id);
     }
 
     @Override
-    public Owner save(Owner object) {
+    public Vet save(Vet object) {
         return super.save(object.getId(), object);
     }
 
     @Override
-    public void delete(Owner object) {
+    public void delete(Vet object) {
        super.delete(object);
     }
 
