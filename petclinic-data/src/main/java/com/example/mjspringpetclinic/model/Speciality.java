6fb25f37bcbae06c3a9b8eb9e6aceb554861e6c1
JosephineAkello <josephineakello2030@gmail.com>
2020-06-10 04:35:28 +0300

    associate vet with the specialities

--- petclinic-data/src/main/java/com/example/mjspringpetclinic/model/Speciality.java
+++ petclinic-data/src/main/java/com/example/mjspringpetclinic/model/Speciality.java
@@ -1,4 +1,14 @@
 package com.example.mjspringpetclinic.model;
 
-public class Speciality {
+public class Speciality extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- petclinic-data/src/main/java/com/example/mjspringpetclinic/model/Vet.java
+++ petclinic-data/src/main/java/com/example/mjspringpetclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package com.example.mjspringpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialitySet() {
+        return specialities;
+    }
+
+    public void setSpecialitySet(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
