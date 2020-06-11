4b0bac04136f9da60a04d9a4e4e0eee55e1c5d22
JosephineAkello <josephineakello2030@gmail.com>
2020-06-11 21:07:52 +0300

    create speciality service

---
+++ petclinic-data/src/main/java/com/example/mjspringpetclinic/service/SpecialityService.java
@@ -0,0 +1,7 @@
+package com.example.mjspringpetclinic.service;
+
+import com.example.mjspringpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality,Long>{
+
+}
