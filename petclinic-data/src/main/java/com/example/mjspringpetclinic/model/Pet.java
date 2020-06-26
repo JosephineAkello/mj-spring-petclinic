766ec869be38d5665b200c8425746406ddfd084c
JosephineAkello <josephineakello2030@gmail.com>
2020-06-26 11:01:16 +0300

    add owners to jpa entity

--- petclinic-data/src/main/java/com/example/mjspringpetclinic/model/Pet.java
+++ petclinic-data/src/main/java/com/example/mjspringpetclinic/model/Pet.java
@@ -1,12 +1,28 @@
 package com.example.mjspringpetclinic.model;
 
 import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name ="pets")
 public class Pet extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name ="birth_date")
     private LocalDateTime birthdate;
 
     public PetType getPetType() {
